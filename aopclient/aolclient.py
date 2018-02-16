2eb3398bcc854226837d79092852b9af1b44acfd
Matt Artingstall <matt@imagndesign.com>
2018-02-16 12:13:54 -0800

    Revised whitelist update

--- aopclient/aolclient.py
+++ aopclient/aolclient.py
@@ -174,7 +174,6 @@ class AOLClient:
     response = self._send_request(url, self.authorized_headers, method="DELETE", data=data)
     return self.__get_response_object(response, data)
 
-
   def get_flight_by_id(self, org_id=0, ad_id=0, campaign_id=0, tactic_id=0, flight_id=0):
     url = "https://{0}/advertiser/campaign-management/v1/organizations/{1}/advertisers/{2}/campaigns/{3}/tactics/{4}/flights/{5}".format(self.one_host, org_id, ad_id, campaign_id, tactic_id, flight_id)
     response = self._send_request(url, self.authorized_headers, method="GET")
@@ -247,13 +246,6 @@ class AOLClient:
         url = url + "?limit={0}&offset={1}".format(limit, offset)        
     response = self._send_request(url, self.authorized_headers, method="GET")
     return self.__get_response_object(response)
-      
-  def get_domains_by_whitelist(self, org_id=0, ad_id=0, whitelist_id=0, limit=0, offset=0):
-    url = "https://{0}/advertiser/inventory-management/v1/organizations/{1}/advertisers/{2}/blacklists/{3}/domains".format(self.one_host, org_id, ad_id, whitelist_id, limit, offset)
-    if limit > 0 or offset > 0:
-        url = url + "?limit={0}&offset={1}".format(limit, offset)        
-    response = self._send_request(url, self.authorized_headers, method="GET")
-    return self.__get_response_object(response)
 
   def get_whitelists_by_tactic(self, org_id=0, ad_id=0, campaign_id=0, tactic_id=0, limit=0, offset=0):      
     url = "https://{0}/advertiser/campaign-management/v1/organizations/{1}/advertisers/{2}/campaigns/{3}/tactics/{4}/whitelists".format(self.one_host, org_id, ad_id, campaign_id, tactic_id, limit, offset)
@@ -290,6 +282,35 @@ class AOLClient:
     response = self._send_request(url, self.authorized_headers, method="PATCH", data=json.dumps(data_list))
     return self.__get_response_object(response, data_list)
 
+  def update_whitelist(self, org_id=0, ad_id=0, whitelist_id=0, domains=[], apps=[]):
+    url = "https://{0}/advertiser/inventory-management/v1/organizations/{1}/advertisers/{2}/whitelists/{3}".format(self.one_host, org_id, ad_id, whitelist_id)
+    current_domains = json.loads(self.get_domains_by_whitelist(org_id, ad_id, whitelist_id))
+    current_apps = json.loads(self.get_apps_by_whitelist(org_id, ad_id, whitelist_id))
+
+    data = []
+    for domain in current_domains.get('data').get('data'):
+      row = {}
+      row["path"] = "/domains/" + str(domain)
+      if domain not in current_list:
+        row["op"] = "replace"
+      else:
+        row["op"] = "add"
+
+      data.append(row)
+
+    for app in current_apps.get('data').get('data'):
+      row = {}
+      row["path"] = "/apps/" + str(app)
+      if domain not in current_list:
+        row["op"] = "replace"
+      else:
+        row["op"] = "add"
+
+      data.append(row)
+
+    response = self._send_request(url, self.authorized_headers, method="PATCH", data=json.dumps(data))
+    return self.__get_response_object(response, data)
+
   def update_tactics_whitelist(self, org_id=0, ad_id=0, campaign_id=0, tactic_id=0, whitelist_id=0):
     url = "https://{0}/advertiser/campaign-management/v1/organizations/{1}/advertisers/{2}/campaigns/{3}/tactics/{4}/whitelists".format(self.one_host, org_id, ad_id, campaign_id, tactic_id)
     data = {}
@@ -299,6 +320,13 @@ class AOLClient:
 
   def get_apps_by_whitelist(self, org_id=0, ad_id=0, whitelist_id=0, limit=0, offset=0):
     url = "https://{0}/advertiser/inventory-management/v1/organizations/{1}/advertisers/{2}/whitelists/{3}/apps".format(self.one_host, org_id, ad_id, whitelist_id, limit, offset)
+    if limit > 0 or offset > 0:
+        url = url + "?limit={0}&offset={1}".format(limit, offset)        
+    response = self._send_request(url, self.authorized_headers, method="GET")
+    return self.__get_response_object(response)
+
+  def get_domains_by_whitelist(self, org_id=0, ad_id=0, whitelist_id=0, limit=0, offset=0):
+    url = "https://{0}/advertiser/inventory-management/v1/organizations/{1}/advertisers/{2}/whitelists/{3}/domains".format(self.one_host, org_id, ad_id, whitelist_id, limit, offset)
     if limit > 0 or offset > 0:
         url = url + "?limit={0}&offset={1}".format(limit, offset)        
     response = self._send_request(url, self.authorized_headers, method="GET")
